62a99892a7b331fe0b7df3fc1247e42c8ac1fe12
Afonso-Lagedo <afonso.lagedo@gmail.com>
2026-09-10 19:08:39 -0300

    nova rota

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express()
 
+app.get('/', (req, res) =>{
+    res.send("Rota padrão")
+})
+
 app.get('/teste', (req, res) =>{
-    res.send("Minha primeira API")
+    res.send("Rota teste")
 })
 
 app.listen(3003, ()=>{
